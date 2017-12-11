c40ea6b51de6489687860904ba53b8a330421afd
huzhengkai <huzhengkai1@163.com>
2017-12-11 17:47:31 +0800

    change

---
+++ src/com/huzhengkai/one/Main.java
@@ -0,0 +1,17 @@
+package com.huzhengkai.one;
+
+/**
+ * Created by root on 2017/12/11.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MyThread t = new MyThread();
+        t.start();
+        for (int i = 0; i < 10000; i++)
+        {
+            System.out.print("Good!");
+        }
+    }
+}
